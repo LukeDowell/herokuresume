873590e2ab0e1793969eab771f238183247b1f70
Luke Dowell <lukedowell@dootdoot.local>
2015-07-29 17:33:59 -0500

    Unbreaks code maybe

--- server/app.js
+++ server/app.js
@@ -8,8 +8,9 @@ var app = express();
 app.set('port', 5000);
 
 app.get("/*", function(req, res) {
+    console.log("Serving page");
     var file = req.params[0] || "views/index.html";
-    res.sendFile(path.join(__dirname, "./public", file));
+    res.sendFile(path.join(__dirname, "./public/", file));
 });
 
 var server = app.listen(app.get('port'), function() {

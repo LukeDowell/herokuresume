8d58bd151d9d0f5fc8f18fc2f3d49e518d98584f
Luke Dowell <lukedowell@dootdoot.local>
2015-07-29 18:04:38 -0500

    Adds environmental port assignment

--- server/app.js
+++ server/app.js
@@ -5,10 +5,9 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.set('port', 5000);
+app.set('port', (process.env.PORT || 5000));
 
 app.get("/*", function(req, res) {
-    console.log("Serving page");
     var file = req.params[0] || "./views/index.html";
     res.sendFile(path.join(__dirname, "./public/", file));
 });
